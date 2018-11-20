86fbd0646b3667db38e51a19dd60ee23b6f5c1b8
Nguyen Phong Nhat <n001417@pc-001417-1.local>
2018-11-20 15:53:22 +0700

    change eslint config

--- src/adapter.js
+++ src/adapter.js
@@ -1,29 +1,27 @@
-let adapter = {}
+const adapter = {};
 const isBrowser = function() {
-    try {
-        return this === window;
-    }
-    catch(e) {
-        return false;
-    }
-}
+  try {
+    return (this === window);
+  } catch (e) {
+    return false;
+  }
+};
 const isNode = function() {
-    try {
-        return this === global;
-    }
-    catch(e) {
-        return false;
-    }
-}
+  try {
+    return this === global;
+  } catch (e) {
+    return false;
+  }
+};
 
 adapter.detectEnvironment = () => {
-    if (isBrowser()) {
-        return 'Browser'
-    }
-    if (isNode()) {
-        return 'Node'
-    }
-    return 'Base'
-}
+  if (isBrowser()) {
+    return 'Browser';
+  }
+  if (isNode()) {
+    return 'Node';
+  }
+  return 'Base';
+};
 
 module.exports = adapter;
