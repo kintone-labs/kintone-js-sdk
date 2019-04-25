0b90216709da957e6cf35071988639def8e8fc17
youtrs <suzuki_yutaro@comture.com>
2019-04-25 16:13:16 +0900

    add test case to upsertRecord.test

--- test/base/module/record/upsertRecord.test.js
+++ test/base/module/record/upsertRecord.test.js
@@ -8,10 +8,10 @@
  */
 
 const nock = require('nock');
-const { API_ROUTE, URI } = require('../../../utils/constant');
+const {API_ROUTE, URI} = require('../../../utils/constant');
 const common = require('../../../utils/common');
 
-const { Connection, Auth, Record, KintoneAPIException } = require(common.MAIN_PATH_BASE);
+const {Connection, Auth, Record, KintoneAPIException} = require(common.MAIN_PATH_BASE);
 // const RECORDS_ROUTE = '/k/v1/records.json';
 
 
@@ -24,7 +24,7 @@ describe('getRecords function', () => {
     it('should return a promise', () => {
       nock(URI)
         .get(`${API_ROUTE.RECORDS}`)
-        .reply(200, { records: [] });
+        .reply(200, {records: []});
       const getRecordsResult = recordModule.getRecords();
       expect(getRecordsResult).toHaveProperty('then');
       expect(getRecordsResult).toHaveProperty('catch');
@@ -141,6 +141,90 @@ describe('getRecords function', () => {
         .then(rsp => {
           expect(rsp.revision).toBe('1');
           expect(rsp.id).toBe('100');
+        });
+    });
+    it('[case 5] can update record', () => {
+      const updateKey = {
+        field: 'Text_0',
+        value: 1234
+      };
+      const recordData = {
+        Number: {
+          value: 1
+        }
+      };
+      const body = {
+        app: 844,
+        query: `${updateKey.field} = "${updateKey.value}"`,
+        fields: [updateKey.field],
+        totalCount: false,
+      };
+      const data = {
+        'app': 844,
+        'updateKey': updateKey,
+        'revision': 2,
+        'record': recordData
+      };
+      nock(URI)
+        .get(`${API_ROUTE.RECORDS}?app=${body.app}&query=${encodeURIComponent(body.query)}&fields[0]=${body.fields[0]}&totalCount=${body.totalCount}`)
+        .matchHeader(common.PASSWORD_AUTH, (authHeader) => {
+          expect(authHeader).toBe(Buffer.from(common.USERNAME + ':' + common.PASSWORD).toString('base64'));
+          return true;
+        })
+        .reply(200, {
+          'records': [recordData]
+        });
+      nock(URI)
+        .put(`${API_ROUTE.RECORD}?app=${body.app}&query=${encodeURIComponent(body.query)}&fields[0]=${body.fields[0]}&totalCount=${body.totalCount}`,
+          (rqBody) => {
+            expect(rqBody.record).toEqual(data.record);
+            return rqBody.app === body.app;
+          })
+        .matchHeader(common.PASSWORD_AUTH, (authHeader) => {
+          expect(authHeader).toBe(common.getPasswordAuth(common.USERNAME, common.PASSWORD));
+          return true;
+        })
+        .matchHeader('Content-Type', (type) => {
+          expect(type).toEqual(expect.stringContaining('application/json;charset=utf-8'));
+          return true;
+        })
+        .reply(200, {'id': '100', 'revision': '1'});
+      return recordModule.upsertRecord(body.app, updateKey, recordData)
+        .then(rsp => {
+          expect(rsp).toHaveProperty('revision');
+        });
+    });
+  });
+  describe('error case', () => {
+    it('[case 6] should return error when getting multi records by updateKey', () => {
+      const updateKey = {
+        field: 'Text_0',
+        value: 1234
+      };
+      const recordData = {
+        Number: {
+          value: 1
+        }
+      };
+      const body = {
+        app: 844,
+        query: `${updateKey.field} = "${updateKey.value}"`,
+        fields: [updateKey.field],
+        totalCount: false,
+      };
+      const message = `${updateKey.field} is not unique field`;
+      nock(URI)
+        .get(`${API_ROUTE.RECORDS}?app=${body.app}&query=${encodeURIComponent(body.query)}&fields[0]=${body.fields[0]}&totalCount=${body.totalCount}`)
+        .matchHeader(common.PASSWORD_AUTH, (authHeader) => {
+          expect(authHeader).toBe(Buffer.from(common.USERNAME + ':' + common.PASSWORD).toString('base64'));
+          return true;
+        })
+        .reply(200, {
+          'records': [recordData, recordData]
+        });
+      return recordModule.upsertRecord(body.app, updateKey, recordData)
+        .catch((err) => {
+          expect(err.message).toEqual(message);
         });
     });
   });
