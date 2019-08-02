563a928f3f2b9c0259ccd69b651ccad9860ac03c
minhhy2801 <minhhy2897@gmail.com>
2019-08-02 09:49:21 +0700

    browser connection change object

--- src/browser/module/connection/Connection.js
+++ src/browser/module/connection/Connection.js
@@ -5,15 +5,19 @@ import * as kintoneBaseJSSDK from '../../../base/main';
 export class Connection extends kintoneBaseJSSDK.Connection {
 
   /**
-     * @param {kintoneBaseJSSDK.Auth} auth
-     * @param {Integer} guestSpaceID
+     * @param {Object} params
+     * @param {kintoneBaseJSSDK.Auth} params.auth
+     * @param {Integer} params.guestSpaceID
      */
-  constructor(auth, guestSpaceID) {
+  constructor({auth, guestSpaceID}) {
     if (auth instanceof kintoneBaseJSSDK.Auth) {
-      super(window.location.host, auth, guestSpaceID);
+      const host = window.location.host;
+      super({host, auth, guestSpaceID});
       this.kintoneAuth = auth;
     } else {
-      super(window.location.host, new kintoneBaseJSSDK.Auth(), guestSpaceID);
+      const host = window.location.host;
+      const basicAuth = new kintoneBaseJSSDK.Auth();
+      super({host, basicAuth, guestSpaceID});
       this.kintoneAuth = undefined;
     }
     this.headers = [];
