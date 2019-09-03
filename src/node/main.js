05926be93016e6a1522fba80b27ef88372a0346f
Huong Pham <phamgiahuong2912@gmail.com>
2019-09-03 11:53:43 +0700

    refactor

--- src/node/main.js
+++ src/node/main.js
@@ -2,7 +2,7 @@
  * kintone api - nodejs client
  * kintone-nodeks-SDK
  */
-import  Auth  from "./authentication/Auth";
+import Auth from "./authentication/Auth";
 import File from './module/file/File';
 import Connection from './connection/Connection';
 import { App, Record, BulkRequest, KintoneAPIException, RecordCursor } from "../base/main";
