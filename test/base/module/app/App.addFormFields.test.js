d271c42cb28a35b24925cac6c6e945ee9056aa5c
minhhy2801 <minhhy2897@gmail.com>
2019-10-16 16:59:05 +0700

    Resolved addFormFields

--- test/base/module/app/App.addFormFields.test.js
+++ test/base/module/app/App.addFormFields.test.js
@@ -60,10 +60,30 @@ describe('Checking App.addFormFields', () => {
 
   it('verify call app function without params', () => {
     const appModule = createAppToSendRequest();
-    return appModule.addFormFields().then((resp) => {
-      // TODO: verify the resp
-    }).catch((error) => {
-      // TODO: verify the error
-    });
+    const expectResult = {
+      'code': 'CB_IL02',
+      'id': 'fY0nuklF16LsztA9FfM0',
+      'message': 'Illegal request.'
+    };
+    nock(URI)
+      .post(APP_FORM_FIELD_PREVIEW_API_ROUTE, (rqBody) => {
+        expect(rqBody).toEqual({});
+        return true;
+      })
+      .matchHeader(PASSWORD_AURH_HEADER, (authHeader) => {
+        expect(authHeader).toBe(createPasswordAuthToCheck());
+        return true;
+      })
+      .matchHeader('Content-Type', (type) => {
+        expect(type).toEqual(expect.stringContaining('application/json'));
+        return true;
+      })
+      .reply(520, expectResult);
+
+    return appModule.addFormFields()
+      .catch((error) => {
+        // (Resolved)TODO: verify the error
+        expect(error.errorResponse).toMatchObject(expectResult);
+      });
   });
 });
