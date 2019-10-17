0b315c9039128870cbb97ceff4133d465f0c90fd
minhhy2801 <minhhy2897@gmail.com>
2019-10-17 08:56:22 +0700

    getFormFields resolved todo

--- test/base/module/app/App.getFormFields.test.js
+++ test/base/module/app/App.getFormFields.test.js
@@ -83,10 +83,22 @@ describe('Checking App.getFormFields', () => {
 
   it('verify call app function without params', () => {
     const appModule = createAppToSendRequest();
-    return appModule.getFormFields().then((resp) => {
-      // TODO: verify the resp
-    }).catch((error) => {
-      // TODO: verify the error
-    });
+    const expectResult = {
+      'code': 'CB_IL02',
+      'id': 'fY0nuklF16LsztA9FfM0',
+      'message': 'Illegal request.'
+    };
+    nock(URI)
+      .get(APP_FORM_FIELD_API_ROUTE)
+      .matchHeader(PASSWORD_AURH_HEADER, (authHeader) => {
+        expect(authHeader).toBe(createPasswordAuthToCheck());
+        return true;
+      })
+      .reply(520, expectResult);
+    return appModule.getFormFields()
+      .catch((error) => {
+        // (Resolved)TODO: verify the error
+        expect(error.errorResponse).toMatchObject(expectResult);
+      });
   });
 });
