43a0d6490906b03c2b48dfb6bd92d0aae2968f2c
suzuki yutaro <37653500+youtr-s@users.noreply.github.com>
2019-10-18 19:08:16 +0900

    bug fix

--- src/browser/module/file/File.js
+++ src/browser/module/file/File.js
@@ -1,4 +1,4 @@
-import common from '../..../base/utils/Common';
+import common from '../../../base/utils/Common';
 import {Connection} from '../connection/Connection';
 import FileModel from '../../../base/model/file/FileModels';
 import KintoneAPIException from '../../../base/main';
