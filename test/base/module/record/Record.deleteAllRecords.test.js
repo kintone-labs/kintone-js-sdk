f4b0bfcab301e50a752e150d8930c1e193859147
Nhat Nguyen <phongnhat19@gmail.com>
2019-10-18 13:49:12 +0700

    add expectedError for deleteAllRecords

--- test/base/module/record/Record.deleteAllRecords.test.js
+++ test/base/module/record/Record.deleteAllRecords.test.js
@@ -3,7 +3,6 @@ import Connection from '../../../../src/node/connection/Connection';
 import Record from '../../../../src/base/module/record/Record';
 import {URI, USERNAME, PASSWORD, DOMAIN} from './common';
 import nock from 'nock';
-import KintoneAPIException from '../../../../src/base/exception/KintoneAPIException';
 
 const auth = new Auth();
 auth.setPasswordAuth({username: USERNAME, password: PASSWORD});
@@ -14,9 +13,29 @@ const BULK_REQUEST_API_ROUTE = '/k/v1/bulkRequest.json';
 
 describe('Check Record.deleteAllRecords', () => {
   it('should throw error when called with invalid app ID', () => {
+    const expectedErrors = {
+      'results': [
+        {
+          'id': 'Jt2jVNMHlZxXPufVeZCz',
+          'code': 'CB_VA01',
+          'message': 'Missing or invalid input.',
+          'errors': {
+            'id': {
+              'messages': [
+                'must be greater than or equal to 1'
+              ]
+            }
+          }
+        }
+      ]
+    };
+    nock(URI)
+      .post(BULK_REQUEST_API_ROUTE)
+      .reply(400, expectedErrors);
     return recordModule.deleteAllRecords(-1, []).catch((err)=>{
       // TODO: deleteAllRecords line 380 is unnecessary
-      expect(err).toBeInstanceOf(KintoneAPIException);
+      expect(Array.isArray(err)).toBe(true);
+      expect(err[0].errorResponse).toMatchObject(expectedErrors.results[0]);
     });
   });
 
